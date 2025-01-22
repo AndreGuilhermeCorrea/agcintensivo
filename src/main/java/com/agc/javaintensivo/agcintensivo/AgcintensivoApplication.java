bf976bcf295f488eb215663a48d8d8ae1473a0d3
AndreGuilhermeCorrea <de_correa@msn.com>
2025-01-22 12:34:19 -0300

    🎉 init: Initial commit Java Spring Boot

---
+++ src/main/java/com/agc/javaintensivo/agcintensivo/AgcintensivoApplication.java
@@ -0,0 +1,13 @@
+package com.agc.javaintensivo.agcintensivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgcintensivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgcintensivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agc/javaintensivo/agcintensivo/AgcintensivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agc.javaintensivo.agcintensivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgcintensivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
