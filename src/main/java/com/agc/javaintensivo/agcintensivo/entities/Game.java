0a395ea7b80b7e4b27e6d4184c9ef3e6e80c1962
AndreGuilhermeCorrea <de_correa@msn.com>
2025-01-22 13:54:51 -0300

    🧪 test: Testing DB

--- src/main/java/com/agc/javaintensivo/agcintensivo/entities/Game.java
+++ src/main/java/com/agc/javaintensivo/agcintensivo/entities/Game.java
@@ -20,13 +20,16 @@ public class Game {
 
     @Column(name = "game_year")
     private Integer year;
+    
 	private String genre;
 	private String platforms;
 	private Double score;
 	private String imgUrl;
-    private String shortDescription;
-    private String longDescription;
 
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
 
     public Game() {
     }
