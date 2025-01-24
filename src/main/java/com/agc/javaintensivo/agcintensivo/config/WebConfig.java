95f48ebed0f81971fcb75540fba22392fac9aa7a
AndreGuilhermeCorrea <de_correa@msn.com>
2025-01-24 17:44:48 -0300

    ✨ feat: CORS

---
+++ src/main/java/com/agc/javaintensivo/agcintensivo/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.agc.javaintensivo.agcintensivo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//Classe de configuração para habilitar o CORS
+@Configuration
+public class WebConfig {
+
+	@Value("${cors.origins}")
+	private String corsOrigins;
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+			}
+		};
+	}
+	
+}
+
+// Fim do código
