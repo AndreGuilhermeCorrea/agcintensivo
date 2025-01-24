6590fa7683b6e886a326f8cbd3c04929c79f1d3c
AndreGuilhermeCorrea <de_correa@msn.com>
2025-01-24 14:48:53 -0300

    🔧 chore: Configuração GameController

--- src/main/java/com/agc/javaintensivo/agcintensivo/controllers/GameController.java
+++ src/main/java/com/agc/javaintensivo/agcintensivo/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.agc.javaintensivo.agcintensivo.dto.GameDTO;
 import com.agc.javaintensivo.agcintensivo.dto.GameMiniDTO;
 import com.agc.javaintensivo.agcintensivo.services.GameService;
 
@@ -15,9 +17,18 @@ import com.agc.javaintensivo.agcintensivo.services.GameService;
 @RequestMapping(value = "/games")
 public class GameController {
    
+    //Injeção de dependência
     @Autowired
     private GameService gameService;
 
+    //Método para buscar um jogo por id
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
+
+    //Método para buscar todos os jogos cadastrados em miniatura
     @GetMapping
     public List<GameMiniDTO> findAll(){
         List<GameMiniDTO> result = gameService.findAll();
